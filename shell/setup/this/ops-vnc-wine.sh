55fb94fc928f6b80473701c74ba0e0cc8283b8a9
x3193 <xcy6272003@126.com>
2017-09-25 18:54:50 +0800

    ubt1404

--- shell/setup/this/ops-vnc-wine.sh
+++ shell/setup/this/ops-vnc-wine.sh
@@ -215,6 +215,28 @@ echo "------------------------Clean--------------------"
 sudo apt-get autoremove -y  
 sudo apt-get clean -y  
 sudo apt-get autoclean -y  
-echo "--------------------------------------------"  
+echo "------------------------update--------------------"  
+if [ "${AUTHORIZED_KEYS}" != "**None**" ]; then
+    echo "=> Found authorized keys"
+    mkdir -p /root/.ssh
+    chmod 700 /root/.ssh
+    touch /root/.ssh/authorized_keys
+    chmod 600 /root/.ssh/authorized_keys
+    IFS=$'\n'
+    arr=$(echo ${AUTHORIZED_KEYS} | tr "," "\n")
+    for x in $arr
+    do
+        x=$(echo $x |sed -e 's/^ *//' -e 's/ *$//')
+        cat /root/.ssh/authorized_keys | grep "$x" >/dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            echo "=> Adding public key to /root/.ssh/authorized_keys: $x"
+            echo "$x" >> /root/.ssh/authorized_keys
+        fi
+    done
+fi
 
-/ops-run.sh full
+if [ ! -f /.root_pw_set ]; then
+	/var/www/html/shell/loader/this/set_root_pw.sh
+fi
+
+echo "--------------------------------------------" 
